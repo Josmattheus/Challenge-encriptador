11dcee32d30454fb43ab480d95844dcc335ac343
Joseph_Matthew <newprovidentia@gmail.com>
2024-03-04 18:35:08 -0500

    arreglo menro del html y re-hacer el js

--- app.js
+++ app.js
@@ -1,110 +1,69 @@
-let mensajeNoEnc = document.getElementById('parrafo-sin-encriptar');
-let mensajeEnc = document.getElementById('parrafo-encriptado');
-let mensaje = "";
+function procesar(opcion) {
+    let textoSinEncriptar = document.getElementById('parrafo-sin-encriptar').value.toLowerCase();
+    let textoEncriptado = document.getElementById('parrafo-encriptado').value.toLowerCase();
+
+    if (opcion == 'encriptar'){
+        let resultado = encriptarLetras(textoSinEncriptar);
+        document.getElementById('parrafo-encriptado').value = resultado;
+    }
+    else{
+        let resultado = desencriptarLetras(textoEncriptado);
+        document.getElementById('parrafo-sin-encriptar').value = resultado;
+    }
+}
+
 
 function encriptarLetras(mensaje) {
-    let contador = 0;
+    let resultado = '';
 
-    while (mensaje.indexOf('a') > -1 || mensaje.indexOf('e') > -1 || mensaje.indexOf('i') > -1 || mensaje.indexOf('o') > -1 || mensaje.indexOf('u') > -1) {
-        if (mensaje[contador] === 'a') {
-            mensaje = mensaje.substring(0, contador) + 'ai' + mensaje.substring(contador + 1);
-            contador += 2;
-        } else if (mensaje[contador] === 'e') {
-            mensaje = mensaje.substring(0, contador) + 'enter' + mensaje.substring(contador + 1);
-            contador += 5;
-        } else if (mensaje[contador] === 'i') {
-            mensaje = mensaje.substring(0, contador) + 'imes' + mensaje.substring(contador + 1);
-            contador += 4;
-        } else if (mensaje[contador] === 'o') {
-            mensaje = mensaje.substring(0, contador) + 'ober' + mensaje.substring(contador + 1);
-            contador += 4;
-        } else if (mensaje[contador] === 'u') {
-            mensaje = mensaje.substring(0, contador) + 'ufat' + mensaje.substring(contador + 1);
-            contador += 4;
+    for (let i = 0; i < mensaje.length; i++) {
+        switch (mensaje[i]) {
+            case 'a':
+                resultado += 'ai';
+                break;
+            case 'e':
+                resultado += 'enter';
+                break;
+            case 'i':
+                resultado += 'imes';
+                break;
+            case 'o':
+                resultado += 'ober';
+                break;
+            case 'u':
+                resultado += 'ufat';
+                break;
+            default:
+                resultado += mensaje[i];
         }
-
-        contador++;
     }
 
-    return mensaje;
+    return resultado;
 }
 
 function desencriptarLetras(mensaje) {
-    let contador = 0;
+    let resultado = '';
 
-    while (mensaje.indexOf('ai') > -1 || mensaje.indexOf('enter') > -1 || mensaje.indexOf('imes') > -1 || mensaje.indexOf('ober') > -1 || mensaje.indexOf('ufat') > -1) {
-        if (mensaje.substring(contador, contador + 2) === 'ai') {
-            mensaje = mensaje.substring(0, contador) + 'a' + mensaje.substring(contador + 2);
-            contador++;
-        } else if (mensaje.substring(contador, contador + 5) === 'enter') {
-            mensaje = mensaje.substring(0, contador) + 'e' + mensaje.substring(contador + 5);
-            contador++;
-        } else if (mensaje.substring(contador, contador + 4) === 'imes') {
-            mensaje = mensaje.substring(0, contador) + 'i' + mensaje.substring(contador + 4);
-            contador++;
-        } else if (mensaje.substring(contador, contador + 4) === 'ober') {
-            mensaje = mensaje.substring(0, contador) + 'o' + mensaje.substring(contador + 4);
-            contador++;
-        } else if (mensaje.substring(contador, contador + 4) === 'ufat') {
-            mensaje = mensaje.substring(0, contador) + 'u' + mensaje.substring(contador + 4);
-            contador++;
+    for (let i = 0; i < mensaje.length; i++) {
+        if (mensaje.substring(i, i + 2) === 'ai') {
+            resultado += 'a';
+            i++;
+        } else if (mensaje.substring(i, i + 5) === 'enter') {
+            resultado += 'e';
+            i += 4;
+        } else if (mensaje.substring(i, i + 4) === 'imes') {
+            resultado += 'i';
+            i += 3;
+        } else if (mensaje.substring(i, i + 4) === 'ober') {
+            resultado += 'o';
+            i += 3;
+        } else if (mensaje.substring(i, i + 4) === 'ufat') {
+            resultado += 'u';
+            i += 3;
+        } else {
+            resultado += mensaje[i];
         }
-
-        contador++;
     }
 
-    return mensaje;
+    return resultado;
 }
-
-
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.getElementById(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
-//iterativa que revisa si la textbox de texto sin ecnriptar esta vacia o no
-function revisarCaja(parrafoId){
-    if (mensajeEnc == ""){ //si parrafo vacio, desactiva el boton
-        document.getElementById(parrafoId).setAttribute('disabled');
-    }
-}
-
-function encriptar(){
-    let mensaje = mensajeNoEnc.value
-    //caso mensaje vacio
-    if (mensaje == "")
-    return;
-
-    else{
-        asignarTextoElemento('parrafo-encriptado',`${asignarTextoElemento(encriptarLetras(mensajeNoEnc))}`); //encriptar el parrafo
-        revisarCaja('boton-encriptar');
-        return;
-    }
-}
-
-function desencriptar(){
-    let mensaje = mensajeEnc.value
-    //caso mensaje vacio
-    if (mensaje == "")
-    return;
-
-    else{
-        asignarTextoElemento('parrafo-sin-encriptar',`${asignarTextoElemento(desencriptarLetras(mensajeNoEnc))}`) //desencriptar el parrafo
-        revisarCaja('boton-desencriptar');
-        return;
-    }
-}
-
-revisarCaja('boton-encriptar');
-revisarCaja('boton-desencriptar');
-
-/*
-document.getElementById('boton-encriptar').addEventListener('click', function() {
-    encriptar(mensajeNoEnc.value);
-});
-
-document.getElementById('boton-desencriptar').addEventListener('click', function() {
-    desencriptar(mensajeNoEnc.value);
-});
-*/
